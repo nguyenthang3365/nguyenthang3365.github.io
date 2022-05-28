9d4a5f3ae06e545e6c578373d8a5351fff69ce48
nguyenthang3365 <62829940+nguyenthang3365@users.noreply.github.com>
2022-05-29 01:14:20 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -74,6 +74,15 @@ $(function(){
             scrollTop: top
         },1000);
     });
+     
+         $("#menu ul li:nth-child(9) a").click(function (e) { 
+        var offset = $('#languageprograming').offset();
+        var top = offset.top;
+        e.preventDefault();
+        $("html").animate({
+            scrollTop: top
+        },1000);
+    });
 
 
     $("#top").click(function (e) { 
@@ -98,4 +107,38 @@ $(function(){
         $("#menu ul li a").removeClass("chu")
         }
      });
+     // hieung3d
+     const myTags = [
+        'JavaScript', 'CSS', 'HTML',
+        'C', 'C++', 'React',
+        'Python', 'Java', 'git',
+        'django', 'Node.js', 'OpenCV',
+        'GCP', 'MySQL', 'jQuery',
+    ];
+    
+    var tagCloud = TagCloud('.content', myTags,{
+    
+      // radius in px
+      radius: 250,
+    
+      // animation speed
+      // slow, normal, fast
+      maxSpeed: 'fast',
+      initSpeed: 'fast',
+    
+      // 0 = top
+      // 90 = left
+      // 135 = right-bottom
+      direction: 135,
+      
+      // interact with cursor move on mouse out
+      keep: true
+      
+    });
+    
+    //To change the color of text randomly
+    var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector('.content').style.color = random_color;
+
 });
